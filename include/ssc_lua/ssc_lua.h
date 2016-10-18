7b2203ef5ecb35bd263505d1739a6fd2f1d213f9
rafa <rafa@shodan.(none)>
2016-10-18 21:13:30 +0200

    comment include header

--- include/ssc_lua/ssc_lua.h
+++ include/ssc_lua/ssc_lua.h
@@ -1,22 +1,36 @@
 #ifndef __SSC_LUA_H__
 #define __SSC_LUA_H__
 
-/*this is the data to pass to the similator on "ssc_create" as the
- "simlib_passed_data" parameter */
-
 #include <ssc/simulator/simulator.h>
 
 struct lua_State;
 /*----------------------------------------------------------------------------*/
 typedef int (*ssc_lua_on_init_func) (struct lua_State* state, void* context);
 /*----------------------------------------------------------------------------*/
+/*
+  ssc_lua_passed_data struct: data to pass on the ssc_create 
+  "simlib_passed_data" parameter to initialize "ssc_lua". It containts the next
+  fields:
+
+  "main_script_path":   Path to the simulation script.
+  "before_main_script": This function will be called before running the main
+                        script. As it passes the current "lua_State" it does
+                        allow the user to add 3rd party libraries coded C to 
+                        the Lua enviroment.
+  "after_main_script":  Same as the above function, but running after the
+                        simulation script is loaded. It is intended to let the
+                        user to retrieve data from the final environment. It 
+                        can be used to import Lua files, but this could be done
+                        from the script too.
+  "on_init_context":    void* to forward to the pair of functions above.
+*/
+/*----------------------------------------------------------------------------*/
 typedef struct ssc_lua_passed_data {
-  char const*          main_script_path; /*path to the simulation script*/
+  char const*          main_script_path;
   ssc_lua_on_init_func before_main_script;
   ssc_lua_on_init_func after_main_script;
   void*                on_init_context;
 }
-ssc_lua_passed_data; /*data to pass on the ssc_create "simlib_passed_data"
-                         parameter*/
+ssc_lua_passed_data; /**/
 /*----------------------------------------------------------------------------*/
 #endif /* __SSC_LUA_H__ */
